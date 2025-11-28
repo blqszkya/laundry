2c5be54ddf2e121a53819df752d6e3ff76c4109f
Bilqiis Khairana Dzakiyya <bilqiiskhairana@gmail.com>
2025-11-28 11:15:05 +0700

    tambah alert window

--- admin/pelanggan_hapus.php
+++ admin/pelanggan_hapus.php
@@ -2,5 +2,5 @@
 	include '../koneksi.php';
 	$id = $_GET['id'];
 	mysqli_query($koneksi,"delete from pelanggan where pelanggan_id='$id'");
-	echo "<script>alert('Data akan dihapus'); window.location.href='pelanggan.php'</script>";
+	echo "<script>alert('Data akan dihapus?'); window.location.href='pelanggan.php'</script>";
 ?>
